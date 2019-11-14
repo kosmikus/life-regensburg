c9e33e1196226dd124f06b702920a1bd56eb4098
Andres Loeh <andres@well-typed.com>
2019-11-14 13:35:40 +0100

    Stylistic adaptations to what I used in the Nürnberg talk.

--- src/Life.hs
+++ src/Life.hs
@@ -1,77 +1,108 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE PatternGuards #-}
 {-# OPTIONS_GHC -Wall #-}
 module Main where
 
 import Data.Char
 import Data.List as L
-import Data.Map as M
+import Data.Map.Strict as M
 import Data.Set as S
 import Data.Void
 import Graphics.Gloss
 import System.Environment
+import System.Exit
 import Text.Megaparsec as P
 import Text.Megaparsec.Char hiding (space)
 import Text.Megaparsec.Char.Lexer
+import Text.Read (readMaybe)
 
-type X     = Int
-type Y     = Int
-type Loc   = (X, Y)
+data Position = Pos !Int !Int
+  deriving (Eq, Ord, Show)
+
+newtype World = World (Map Position Age)
+  deriving (Show)
+
 type Age   = Int
-type World = Map Loc Age
 
-isAlive :: World -> Loc -> Bool
-isAlive = flip M.member
+(.+.) :: Position -> Position -> Position
+Pos x1 y1 .+. Pos x2 y2 = Pos (x1 + x2) (y1 + y2)
 
-neighbors :: Loc -> Set Loc
-neighbors (x, y) =
-  S.fromList
-    [ (x0, y0)
-    | x0 <- [x - 1 .. x + 1]
-    , y0 <- [y - 1 .. y + 1]
-    ]
+isAlive :: World -> Position -> Bool
+isAlive (World m) pos =
+  M.member pos m
 
-countLivingNeighbors :: World -> Loc -> Int
-countLivingNeighbors world loc =
-  M.size (M.restrictKeys world (neighbors loc))
+-- | Defaults age of dead position to 0.
+currentAge :: World -> Position -> Int
+currentAge (World m) pos =
+  M.findWithDefault 0 pos m
+
+neighbours :: Position -> Set Position
+neighbours pos =
+  S.map (pos .+.)
+    (S.fromList
+      [ Pos x y
+      | x <- [-1 .. 1]
+      , y <- [-1 .. 1]
+      , not ((x == 0) && (y == 0))
+      ]
+    )
+
+countLivingNeighbours :: World -> Position -> Int
+countLivingNeighbours (World m) pos =
+  M.size (M.restrictKeys m (neighbours pos))
 
 rule :: Bool -> Int -> Bool
-rule True  alive = alive `elem` [3, 4]
+rule True  alive = alive `elem` [2, 3]
 rule False alive = alive == 3
 
-candidates :: World -> Set Loc
-candidates world =
-  S.unions (fmap neighbors (M.keys world))
+candidates :: World -> Set Position
+candidates (World m) =
+  let
+    positionsAlive = M.keysSet m
+  in
+    S.union positionsAlive (S.unions (S.map neighbours positionsAlive))
 
 step :: World -> World
 step world =
-  M.mapMaybeWithKey
-    (\ loc age -> if rule (isAlive world loc) (countLivingNeighbors world loc) then Just (age + 1) else Nothing)
-    (M.fromSet (\ loc -> M.findWithDefault 0 loc world) (candidates world))
+  World
+    (M.mapMaybeWithKey
+      (\ loc age ->
+        if rule (isAlive world loc) (countLivingNeighbours world loc)
+          then Just (age + 1)
+          else Nothing
+      )
+      (M.fromSet (currentAge world) (candidates world))
+    )
+
+-- | Create a world from a list of positions, initialising age to 1.
+makeWorld :: [Position] -> World
+makeWorld positions =
+  World (M.fromList (L.map (\ pos -> (pos, 1)) positions))
 
 glider :: World
 glider =
-    M.fromSet (const 1)
-  $ S.fromList [(0, -1), (1, 0), (-1, 1), (0, 1), (1, 1)]
+  makeWorld
+    [Pos 0 (-1), Pos 1 0, Pos (-1) 1, Pos 0 1, Pos 1 1]
 
-renderBlock :: Loc -> Age -> Picture
-renderBlock (x, y) age =
-  Color
+renderBlock :: Position -> Age -> Picture
+renderBlock (Pos x y) age =
+  color
     (ageToColor age)
       (translate
         (fromIntegral x)
         (fromIntegral (-y))
         (rectangleSolid 0.8 0.8)
-        -- (Polygon [(-0.4,-0.4), (-0.4, 0.4), (0.4, 0.4), (0.4, -0.4)])
       )
 
+-- | Map age to colour. White for new cells, black for
+-- infinitely old cells. Getting darker over time.
+--
 ageToColor :: Age -> Color
-ageToColor 0 = black
-ageToColor 1 = white
-ageToColor n = mixColors 3 (fromIntegral n) white black
+ageToColor n = mixColors 3 (fromIntegral (n - 1)) white black
 
 renderWorld :: World -> Picture
-renderWorld world =
-  Pictures (fmap (uncurry renderBlock) (M.toList world))
+renderWorld (World m) =
+  pictures (fmap (uncurry renderBlock) (M.toList m))
 
 simulateWorld :: Int -> World -> IO ()
 simulateWorld speed model =
@@ -83,24 +114,36 @@ simulateWorld speed model =
     (scale 10 10 . renderWorld) -- prescale by factor of 10
     (\ _viewport _time world -> step world)
 
+-- | Takes two command line arguments, the name of the
+-- rle file to read and the speed.
+--
 main :: IO ()
 main = do
-  [file, rate] <- getArgs
-  world <- fmap readRle (readFile file)
-  simulateWorld (read rate) world
+  args <- getArgs
+  go args
+  where
+    go [file, rate] | Just speed <- readMaybe rate = do
+      contents <- readFile file
+      world <- readRle contents
+      simulateWorld speed world
+    go _ = do
+       putStrLn "Usage: life [filename] [speed]"
+       exitFailure
 
+-- | Read a lif106 format file, which is yet a simpler
+-- format than rle. Not used.
+--
 readLif106 :: String -> World
 readLif106 contents =
-    M.fromSet (const 1)
-  . S.fromList
-  . fmap (\ [x, y] -> (read x, read y))
+    makeWorld
+  . fmap (\ [x, y] -> Pos (read x) (read y))
   . fmap words
   . L.filter (\ l -> L.take 1 l /= "#")
   $ lines contents
 
-data RLEFormat =
-  RLEFormat
-    Loc -- dimensions
+data RLEFile =
+  RLEFile
+    Position -- dimensions (we do not use them)
     [RLEInstr]
 
 data RLEInstr =
@@ -108,30 +151,27 @@ data RLEInstr =
   | RLEDead !Int
   | RLENewline !Int
 
-readRle :: String -> World
+readRle :: String -> IO World
 readRle contents =
   case parse parseRle "input" contents of
-    Left e -> error (errorBundlePretty e)
-    Right w  -> interpretRLEFormat w
+    Left e  -> putStr (errorBundlePretty e) >> exitFailure
+    Right w -> return (rleToWorld w)
 
-interpretRLEFormat :: RLEFormat -> World
-interpretRLEFormat (RLEFormat _ entries) =
-  M.fromSet (const 1) (go (0, 0) S.empty entries)
+rleToWorld :: RLEFile -> World
+rleToWorld (RLEFile _dims instructions) =
+  makeWorld (go (Pos 0 0) instructions)
   where
-    go :: Loc -> Set Loc -> [RLEInstr] -> Set Loc
-    go (!_x, !_y) !world [] = world
-    go (!_x, !y) !world (RLENewline n : instrs) = go (0, y + n) world instrs
-    go (!x, !y) !world (RLEDead n : instrs) = go (x + n, y) world instrs
-    go (!x, !y) !world (RLEAlive n : instrs) = go (x + n, y) world' instrs
+    go :: Position -> [RLEInstr] -> [Position]
+    go _pos      []                      = []
+    go (Pos _ y) (RLENewline n : instrs) = go (Pos 0 (y + n)) instrs
+    go (Pos x y) (RLEDead    n : instrs) = go (Pos (x + n) y) instrs
+    go (Pos x y) (RLEAlive   n : instrs) = positions ++ go (Pos (x + n) y) instrs
       where
-        world' = S.union world (S.fromList [(x', y) | x' <- [x .. x + n - 1]])
+        positions = [ Pos x' y | x' <- [x .. x + n - 1] ]
 
-parseRle :: Parsec Void String RLEFormat
-parseRle = do
-  genspace
-  (x, y) <- parseDimensions
-  instrs <- parseInstrs
-  return (RLEFormat (x, y) instrs)
+parseRle :: Parsec Void String RLEFile
+parseRle =
+  RLEFile <$ genspace <*> parseDimensions <*> parseInstrs
 
 -- | Space but not newline.
 sp1 :: Parsec Void String ()
@@ -160,13 +200,16 @@ headertoken :: String -> Parsec Void String String
 headertoken x =
   lexeme headerspace (string x)
 
--- | Space including newline
+restOfFirstLine :: Parsec Void String ()
+restOfFirstLine =
+  () <$ takeWhileP (Just "rest of line") (\ x -> x /= '\n')
 
-parseDimensions :: Parsec Void String (Int, Int)
+-- Parse the first line
+parseDimensions :: Parsec Void String Position
 parseDimensions =
-  (,)
+  Pos
     <$ headertoken "x" <* headertoken "=" <*> parseInt <* headertoken ","
-    <* headertoken "y" <* headertoken "=" <*> parseInt <* newline <* genspace
+    <* headertoken "y" <* headertoken "=" <*> parseInt <* restOfFirstLine <* newline <* genspace
 
 parseInt :: Parsec Void String Int
 parseInt =
@@ -178,7 +221,11 @@ parseInstrs =
 
 parseInstr :: Parsec Void String RLEInstr
 parseInstr =
-  flip ($) <$> option 1 decimal <*> (RLEAlive <$ gentoken "o" <|> RLEDead <$ gentoken "b" <|> RLENewline <$ gentoken "$")
+  flip ($) <$> option 1 decimal <*>
+    (   RLEAlive   <$ gentoken "o"
+    <|> RLEDead    <$ gentoken "b"
+    <|> RLENewline <$ gentoken "$"
+    )
 
 renderGlider :: IO ()
 renderGlider =
